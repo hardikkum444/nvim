fc67b69b7ec9f2cd3846143ac4f1ef61f4cca28d
Soap_dispenser_4000 <114590291+hardikkum444@users.noreply.github.com>
2024-03-07 19:51:38 +0530

    airline and neotree added

--- lua/man44/packer.lua
+++ lua/man44/packer.lua
@@ -1,12 +1,15 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-
+  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	  --                           , branch = '0.1.x',
+	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
@@ -28,6 +31,21 @@ return require('packer').startup(function(use)
 	  'neoclide/coc-java',
 	 --branch = 'release'
   }
+
+--  use ('preservim/nerdtree')
+
+use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = { 
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+}
+
+use ('vim-airline/vim-airline')
 
 
   end)
