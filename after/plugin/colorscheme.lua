5494def9023c9f2699cf43a993db4a635be98e8f
Soap_dispenser_4000 <114590291+hardikkum444@users.noreply.github.com>
2024-03-07 19:52:28 +0530

    Create colorscheme.lua

---
+++ after/plugin/colorscheme.lua
@@ -0,0 +1,15 @@
+-- Load the solarized-osaka plugin and configure it
+require("solarized-osaka").setup({
+  night_brightness = 1.8, -- Adjust brightness of the colors of the **Day** style
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "dark", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+})
