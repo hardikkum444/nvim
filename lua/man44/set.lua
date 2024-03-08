3eab985773946a0928735ba41bbca929d0537de4
Soap_dispenser_4000 <114590291+hardikkum444@users.noreply.github.com>
2024-03-08 21:00:42 +0530

    Update set.lua

--- lua/man44/set.lua
+++ lua/man44/set.lua
@@ -1,10 +1,12 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+vim.opt.mouse = a
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
+vim.api.nvim_set_option("clipboard","unnamed") 
 
 vim.opt.scrolloff = 8
+vim.o.clipboard = "unnamedplus"
