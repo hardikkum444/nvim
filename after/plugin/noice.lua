9122e07b7ef0322f4336efc51287bcef35568be2
Soap_dispenser_4000 <114590291+hardikkum444@users.noreply.github.com>
2024-03-08 22:58:59 +0530

    Create noice.lua

---
+++ after/plugin/noice.lua
@@ -0,0 +1,50 @@
+  require("noice").setup({
+    views = {
+      cmdline_popup = {
+        position = {
+          row = 5,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = 8,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = 10,
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        },
+      },
+    },
+  })
+
+
+  -- config 2 (clean setup)
+
+    -- require("noice").setup({
+    -- views = {
+    --   cmdline_popup = {
+    --     border = {
+    --       style = "none",
+    --       padding = { 2, 3 },
+    --     },
+    --     filter_options = {},
+    --     win_options = {
+    --       winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+    --     },
+    --   },
+    -- },
+  -- })
