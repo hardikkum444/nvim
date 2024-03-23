dea8542bdcb7f64d82e9503ce98a6c911f2ac476
man44 <114590291+hardikkum444@users.noreply.github.com>
2024-03-23 12:20:38 +0530

    Create colors.lua

---
+++ after/plugin/colors.lua
@@ -0,0 +1,11 @@
+# sets the terminal color scheme and settings to nvim
+
+function TerminalToVim(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+end
+
+TerminalToVim()
