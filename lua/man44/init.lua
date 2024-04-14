a766ac95646d8f699daa01bde474ccb4cb3482b5
man44 <114590291+hardikkum444@users.noreply.github.com>
2024-04-14 20:37:49 +0530

    changing to tokyo-night color theme

--- lua/man44/init.lua
+++ lua/man44/init.lua
@@ -1,7 +1,7 @@
 require("man44.remap")
 require("man44.set")
 
-vim.cmd[[colorscheme solarized-osaka]]
-
+-- vim.cmd[[colorscheme solarized-osaka]]
+vim.cmd[[colorscheme tokyonight-night]]
 -- Key mapping for Coc.nvim Popup Menu Confirmation
 vim.api.nvim_set_keymap('i', '<CR>', [[pumvisible() ? coc#_select_confirm() : "\<CR>"]], { expr = true, noremap = true })
