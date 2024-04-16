05eb4ec3827a020aed47112d69727ae3fea81719
Hardik Kumawat <114590291+hardikkum444@users.noreply.github.com>
2024-04-16 15:02:04 +0530

    adding lsp

--- lua/man44/packer.lua
+++ lua/man44/packer.lua
@@ -35,6 +35,22 @@ return require('packer').startup(function(use)
 --  use ('preservim/nerdtree')
 
 -- neotree
+
+   use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
+  requires = {
+    --- Uncomment the two plugins below if you want to manage the language servers from neovim
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'L3MON4D3/LuaSnip'},
+  }
+}
+
 use {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
