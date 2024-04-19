185b017b47cff31b5798afadafb10e6a37f834bd
Hardik Kumawat <114590291+hardikkum444@users.noreply.github.com>
2024-04-19 09:42:31 +0530

    keybindings

---
+++ minimalSetup/lua/man44/remap.lua
@@ -0,0 +1,15 @@
+local opts = { noremap = true, silent = true }
+
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- select all
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- no need for tab selection remap as I have harpoon
+
+
+-- splitting screen
+vim.keymap.set("n", "ss", ":split<Return>", opts)
+vim.keymap.set("n", "sv", ":vsplit<Return>", opts)
