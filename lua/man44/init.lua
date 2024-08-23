f59a50f362967900c1467b2001f9d66ab07f6657
man44 <114590291+hardikkum444@users.noreply.github.com>
2024-08-23 17:14:08 +0000

    yanking to clip

--- lua/man44/init.lua
+++ lua/man44/init.lua
@@ -5,3 +5,6 @@ require("man44.set")
 vim.cmd[[colorscheme tokyonight-night]]
 -- Key mapping for Coc.nvim Popup Menu Confirmation
 vim.api.nvim_set_keymap('i', '<CR>', [[pumvisible() ? coc#_select_confirm() : "\<CR>"]], { expr = true, noremap = true })
+
+-- Allowing yanking to clipboard
+vim.opt.clipboard = 'unnamedplus'
