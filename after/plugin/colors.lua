2eaa1eaa33626a3ef58c3c5feb37bc8b2c910b99
man44 <114590291+hardikkum444@users.noreply.github.com>
2024-10-03 16:51:09 +0530

    changing the bg color for noice notify as well

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,11 +1,16 @@
-# sets the terminal color scheme and settings to nvim
+-- setting the background to that of terminal
+
+require("notify").setup({
+  background_colour = "#000000",
+})
 
 function TerminalToVim(color)
-    color = color
+    color = color or "default"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 TerminalToVim()
+
