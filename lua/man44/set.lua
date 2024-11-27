019dd4a1f6ff1b1184ce7d1c76296b7a653990e8
hardikkum444 <hardikkumawat444@gmail.com>
2024-11-27 14:40:35 +0530

    adding line highlight

--- lua/man44/set.lua
+++ lua/man44/set.lua
@@ -1,12 +1,12 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
-
-vim.opt.mouse = a
+vim.opt.mouse = "a"
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.api.nvim_set_option("clipboard","unnamed") 
-
+-- vim.api.nvim_set_option("clipboard","unnamed") 
+vim.opt.clipboard = "unnamedplus"
 vim.opt.scrolloff = 8
 vim.o.clipboard = "unnamedplus"
+vim.opt.cursorline = true
